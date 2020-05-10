4b20647b55b741b5659041f13634b7cd904973ed
swoga <swoga@users.noreply.github.com>
2020-05-10 15:38:32 +0200

    add class for discover endpoint YAML structure

---
+++ MikrotikExporter/Discover.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace MikrotikExporter.Discover
+{
+    class StaticConfig
+    {
+        [JsonPropertyName("targets")]
+        public string[] Targets { get; set; }
+        [JsonPropertyName("labels")]
+        public Dictionary<string, string> Lables { get; set; }
+    }
+}
