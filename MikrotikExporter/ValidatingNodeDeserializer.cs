7efcbf5cddcc0f334b368dc71b7bc1f9877d224c
swoga <swoga@users.noreply.github.com>
2020-05-10 15:37:20 +0200

    add YAML Deserializer which validates DataAnnotations

---
+++ MikrotikExporter/ValidatingNodeDeserializer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using YamlDotNet.Core;
+using YamlDotNet.Serialization;
+
+namespace MikrotikExporter
+{
+    public class ValidatingNodeDeserializer : INodeDeserializer
+    {
+        private readonly INodeDeserializer _nodeDeserializer;
+
+        public ValidatingNodeDeserializer(INodeDeserializer nodeDeserializer)
+        {
+            _nodeDeserializer = nodeDeserializer;
+        }
+
+        public bool Deserialize(IParser parser, Type expectedType, Func<IParser, Type, object> nestedObjectDeserializer, out object value)
+        {
+            if (_nodeDeserializer.Deserialize(parser, expectedType, nestedObjectDeserializer, out value))
+            {
+                var context = new ValidationContext(value, null, null);
+                Validator.ValidateObject(value, context, true);
+                return true;
+            }
+            return false;
+        }
+    }
+}
