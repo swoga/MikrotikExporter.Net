8521e0a9117780bd69f8f34b99c7f42597d9aa4f
swoga <swoga@users.noreply.github.com>
2020-08-28 17:22:49 +0200

    feat: add log recording

--- MikrotikExporter/Log.cs
+++ MikrotikExporter/Log.cs
@@ -15,19 +15,47 @@ namespace MikrotikExporter
             Debug2
         }
         public LogLevel Level { get; set; }
-        public static Log Main { get; } = new Log(Array.Empty<string>(), LogLevel.Error);
+        public static Log Main { get; } = new Log(Array.Empty<string>(), LogLevel.Error, null);
         private static readonly object consoleLock = new object();
         private readonly string[] contexts;
+        private readonly List<string> logs;
+        public string[] Logs
+        {
+            get
+            {
+                if (logs == null)
+                {
+                    return null;
+                }
+
+                lock (logs)
+                {
+                    return logs?.ToArray();
+                }
+            }
+        }
 
         private void Write(string type, ConsoleColor color, string message)
         {
+            var part1 = $"{DateTime.Now:yyyy-MM-dd HH:mm:ss.fff} ";
+            var part2 = type;
+            var part3 = $" {String.Join("", contexts.Select(context => $"[{context}]").ToArray())}: {message}";
+
             lock (consoleLock)
             {
-                Console.Write($"{DateTime.Now:yyyy-MM-dd HH:mm:ss.fff} ");
+                Console.Write(part1);
                 Console.ForegroundColor = color;
-                Console.Write(type);
+                Console.Write(part2);
                 Console.ResetColor();
-                Console.WriteLine($" {String.Join("", contexts.Select(context => $"[{context}]").ToArray())}: {message}");
+                Console.WriteLine(part3);
+            }
+
+            if (logs != null)
+            {
+                lock (logs)
+                {
+                    logs.Add($"{part1}{part2}{part3}");
+                }
             }
         }
 
@@ -60,15 +88,18 @@ namespace MikrotikExporter
             }
         }
 
-        private Log(string[] contexts, LogLevel level)
+        private Log(string[] contexts, LogLevel level, List<string> recorder)
         {
             this.contexts = contexts;
             Level = level;
+            logs = recorder;
         }
 
-        public Log CreateContext(string context, LogLevel? forceLevel = null)
+        public Log CreateContext(string context, LogLevel? forceLevel = null, bool record = false)
         {
-            return new Log(contexts.Concat(new string[] { context }).ToArray(), forceLevel ?? Level);
+            bool shouldRecord = record || logs != null;
+            List<string> recorder = shouldRecord ? logs ?? new List<string>() : null;
+            return new Log(contexts.Concat(new string[] { context }).ToArray(), forceLevel ?? Level, recorder);
         }
     }
 }
